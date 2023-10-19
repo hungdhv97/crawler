7986eb575cc08ad5e8420b605a4f52a035aefc59
Hung.Thai (Patrick) /Sotatek/DnU <e.hung.thai@wesang.com>
2023-10-19 17:49:36 +0700

    Custom pipelines

--- story_crawler/pipelines.py
+++ story_crawler/pipelines.py
@@ -5,9 +5,28 @@
 
 
 # useful for handling different item types with a single interface
-from itemadapter import ItemAdapter
+import os
+from urllib.parse import urlparse
+
+import scrapy
+from scrapy.exceptions import DropItem
+from scrapy.pipelines.images import ImagesPipeline
 
 
 class StoryCrawlerPipeline:
     def process_item(self, item, spider):
         return item
+
+
+class CustomImagesPipeline(ImagesPipeline):
+
+    def file_path(self, request, response=None, info=None, *, item=None):
+        path = urlparse(request.url).path
+        return os.path.basename(path)
+
+    def get_media_requests(self, item, info):
+        for image_url in item['image_urls']:
+            if any(extension in image_url.lower() for extension in ['.jpg', '.jpeg', '.png']):
+                yield scrapy.Request(image_url)
+            else:
+                raise DropItem(f'Dropping non-jpg/png image: {image_url}')
