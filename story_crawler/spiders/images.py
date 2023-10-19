a457964b04b15b7bb62b7234f7fbef3fdfcd321f
Hung.Thai (Patrick) /Sotatek/DnU <e.hung.thai@wesang.com>
2023-10-19 17:28:51 +0700

    First commit

---
+++ story_crawler/spiders/images.py
@@ -0,0 +1,14 @@
+import scrapy
+
+from story_crawler.items import StoryCrawlerItem
+
+
+class ImagesSpider(scrapy.Spider):
+    name = 'images_spider'
+    allowed_domains = ['truyenfull.vn']
+    start_urls = ['https://truyenfull.vn/']
+
+    def parse(self, response):
+        item = StoryCrawlerItem()
+        item['image_urls'] = response.css('img::attr(src)').extract()
+        yield item
