503f2d6f80968f7713b7a2295bf69d1aee28fcdc
Hung.Thai (Patrick)/Sotatek/DnU <e.hung.thai@wesang.com>
2023-11-15 17:07:37 +0700

    Use Pipeline to clean item

--- story_crawler/pipelines.py
+++ story_crawler/pipelines.py
@@ -10,6 +10,7 @@ import sqlite3
 
 import cloudinary
 import cloudinary.uploader
+from itemadapter import ItemAdapter
 
 
 class StoryCrawlerPipeline:
@@ -60,3 +61,54 @@ class DataStoragePipeline:
 
     def close_spider(self, spider):
         self.conn.close()
+
+
+class BookScraperPipeline:
+    def process_item(self, item, spider):
+        adapter = ItemAdapter(item)
+        field_names = adapter.field_names()
+
+        for field_name in field_names:
+            if field_name != 'description':
+                value = adapter.get(field_name)
+                adapter[field_name] = value.strip()
+
+        lowercase_keys = ['category', 'product_type']
+        for lowercase_key in lowercase_keys:
+            value = adapter.get(lowercase_key)
+            adapter[lowercase_key] = value.lower()
+
+        price_keys = ['price', 'price_excl_tax', 'price_incl_tax', 'tax']
+        for price_key in price_keys:
+            value = adapter.get(price_key)
+            value = value.replace('£', '')
+            adapter[price_key] = float(value)
+
+        availability_string = adapter.get('availability')
+        split_string_array = availability_string.split('(')
+        if len(split_string_array) < 2:
+            adapter['availability'] = 0
+        else:
+            availability_array = split_string_array[1].split(' ')
+            adapter['availability'] = int(availability_array[0])
+
+        num_reviews_string = adapter.get('num_reviews')
+        adapter['num_reviews'] = int(num_reviews_string)
+
+        stars_string = adapter.get('stars')
+        split_stars_array = stars_string.split(' ')
+        stars_text_value = split_stars_array[1].lower()
+        if stars_text_value == 'zero':
+            adapter['stars'] = 0
+        elif stars_text_value == 'one':
+            adapter['stars'] = 0
+        elif stars_text_value == 'two':
+            adapter['stars'] = 0
+        elif stars_text_value == 'three':
+            adapter['stars'] = 0
+        elif stars_text_value == 'four':
+            adapter['stars'] = 0
+        elif stars_text_value == 'five':
+            adapter['stars'] = 0
+
+        return item
